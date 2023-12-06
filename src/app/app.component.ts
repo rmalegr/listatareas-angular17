21c98d2512e68444c00e43091e805fb5d10c32c3
rmalegr <rodrgiomalegre@gmail.com>
2023-12-06 12:36:36 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TareasService } from './services/tareas.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'listaTareasApp';
+
+  listaTareas: string[] = [];
+  nuevatarea: string = '';
+  private _tareasServices = inject(TareasService);
+
+  ngOnInit(): void {
+    this.listaTareas = this._tareasServices.getTareas();
+  }
 }
